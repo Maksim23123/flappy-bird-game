2bc4e4a8d2d52323ea8f703d792db11fae5b1b7c
Maksim23123 <102969640+Maksim23123@users.noreply.github.com>
2024-03-20 14:59:36 +0100

    Add bg models to the game files

--- Assets/Scripts/Data/GameData.cs
+++ Assets/Scripts/Data/GameData.cs
@@ -6,27 +6,41 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "GameData", menuName = "Data/GameData")]
 public class GameData : ScriptableObject
 {
-    [SerializeField]
-    private int coinsCount = 0;
+    
+    private readonly string COINS_TAG = "coinsCount";
 
-    [SerializeField]
-    private int gemsCount = 0;
+    
+    private readonly string GEMS_TAG = "gemsCount";
 
     public int CoinsCount
     {
-        get => coinsCount;
+        get
+        {
+            if (PlayerPrefs.HasKey(COINS_TAG))
+                return PlayerPrefs.GetInt(COINS_TAG);
+            else
+                return 0;
+        }
+
         set
         {
-            coinsCount = value;
+            PlayerPrefs.SetInt(COINS_TAG, value);
         }
     }
 
     public int GemsCount
     {
-        get => gemsCount;
+        get
+        {
+            if (PlayerPrefs.HasKey(GEMS_TAG))
+                return PlayerPrefs.GetInt(GEMS_TAG);
+            else
+                return 0;
+        }
+
         set
         {
-            gemsCount = value;
+            PlayerPrefs.SetInt(GEMS_TAG, value);
         }
     }
 }
